7b926090da2682acdd227ea02bab816215955cb0
Talismar Fernandes Costa <talismar788.una@gmail.com>
2025-02-18 16:41:58 -0300

    feat: add initial config

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
 driver.get("https://www.google.com")
+
+driver.close()
