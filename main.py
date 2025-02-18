d94e1ce8d106c076f4f3850771af78238a270dc2
Talismar Fernandes Costa <talismar788.una@gmail.com>
2025-02-18 16:46:53 -0300

    feat: add initial config

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+options = FirefoxOptions()
+options.add_argument("--headless")
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
 
 driver.get("https://www.google.com")
 
